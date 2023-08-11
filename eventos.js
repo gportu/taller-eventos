a3a37eb146939a14a2c81a99c3ac70dace8d529b
Gaston <gastonportu14@gmail.com>
2023-08-11 20:48:50 -0300

    hola soy el div

--- eventos.js
+++ eventos.js
@@ -1,3 +1,10 @@
 function alerta(){
     alert("Hola!");
 }
+
+document.getElementById("div").addEventListener("click",funcion);
+
+function funcion(){
+    document.getElementById("div").innerHTML = "Hola, Soy el Div!";
+    
+}
